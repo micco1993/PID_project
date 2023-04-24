e53fc9060707ef4fe8381e93fe6854d787e4f30f
Mikael Dahlbom <mikael.dahlbom93@gmail.com>
2023-04-24 20:42:09 +0300

    Change units for csv file and add instructions for Android app

--- PidController/app/src/main/java/com/mikael/pidcontroller/MainActivity.java
+++ PidController/app/src/main/java/com/mikael/pidcontroller/MainActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a08a4ca8f74d22c1a78ced4b657b5da6cc814dfa5bebcdf1466514d3d4ebab26
+oid sha256:6897e9ea18c7824ab295ac0320acbb8e2e51ecad3e8bbcd6c5899eab1a4d45de
 size 47817
